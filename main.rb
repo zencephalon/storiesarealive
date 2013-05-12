0aafc78a8959f5cbaecd5f824552206b4869f690
Matthew Bunday <zenceph@gmail.com>
2013-05-12 03:54:16 -0400

    individual draft display working now.

--- main.rb
+++ main.rb
@@ -15,7 +15,6 @@ $drafts = $db.collection('drafts')
 DATA_DIR = "./data"
 
 class Draft
-
     def initialize(user, draft, content)
         if $drafts.find_one({:user => user, :draft => draft})
             raise 'Draft already exists'
@@ -25,12 +24,20 @@ class Draft
         @dir = "#{DATA_DIR}/#{@user}/#{@draft}"
         `mkdir #{@dir}`
         `git init #{@dir}`
-        $drafts.insert({:user => @user, :draft => @draft, :dir => @dir})
+        draft_id = $users.find_and_modify(:query => {:user => user}, :update => {"$inc" => {"draft_count" => 1}}, :new => true)
+        $drafts.insert({:user => @user, :draft => @draft, :dir => @dir, :num => draft_id})
         File.open("#{@dir}/draft.textile", 'w') do |f|
             f.puts @content
         end
         `git --git-dir=#{@dir}/.git --work-tree=#{@dir} add draft.textile`
         `git --git-dir=#{@dir}/.git --work-tree=#{@dir} commit -a -m 'initial commit'`
+    end
+
+    def self.get_draft(user, num)
+        draft = $drafts.find_one({:user => user, :num => num})
+        return nil if draft.nil? 
+        draft['content'] = File.open("#{draft['dir']}/draft.textile").read
+        return draft
     end
 
     def self.get_drafts(user)
@@ -76,6 +83,11 @@ end
 
 get "/draft/new", :auth => :user do
     liquid :draft, :locals => { :title => "Write a new draft!" }
+end
+
+get "/draft/:num", :auth => :user do
+    draft = Draft.get_draft(username, params[:num].to_i)
+    liquid :draft_display, :locals => { :draft => draft }
 end
 
 get "/signup" do
