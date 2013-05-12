5233f1d024580fff03c57a5b6193b1619d82cc7b
Matthew Bunday <zenceph@gmail.com>
2013-05-12 00:30:42 -0400

    bringing sinatra in to play.

---
+++ main.rb
@@ -0,0 +1,89 @@
+require 'rubygems'
+require 'bundler/setup'
+
+require 'bcrypt'
+require 'haml'
+require 'sinatra'
+require 'mongo'
+
+enable :sessions
+
+client = Mongo::MongoClient.new('localhost', 27017)
+db = client.db('auth-example')
+users = db.collection('users')
+
+helpers do
+    def login?
+        if session[:username].nil?
+            return false
+        else
+            return true
+        end
+    end
+
+    def username
+        return session[:username]
+    end
+end
+
+get "/" do
+    liquid :index, :locals => { :user => username, :title => "Welcome!" }
+end
+
+get "/signup" do
+    haml :signup
+end
+
+post "/signup" do
+    password_salt = BCrypt::Engine.generate_salt
+    password_hash = BCrypt::Engine.hash_secret(params[:password], password_salt)
+
+    # save into mongodb
+    id = users.insert({
+            :_id => params[:username],
+            :salt => password_salt,
+            :passwordhash => password_hash 
+        })
+
+    session[:username] = params[:username]
+    redirect "/"
+end
+
+get "/login" do
+    liquid :login
+end
+
+post "/login" do
+    if user = users.find_one({:_id => params[:username]})
+        if user["passwordhash"] == BCrypt::Engine.hash_secret(params[:password], user["salt"])
+            session[:username] = params[:username]
+            redirect "/"
+        end
+    end
+    haml :error
+end
+
+get "/logout" do
+    session[:username] = nil
+    redirect "/"
+end
+
+__END__
+@@signup
+%p Enter the username and password!
+%form(action="/signup" method="post")
+  %div
+    %label(for="username")Username:
+    %input#username(type="text" name="username")
+  %div
+    %label(for="password")Password:
+    %input#password(type="password" name="password")
+  %div
+    %label(for="checkpassword")Password:
+    %input#password(type="password" name="checkpassword")
+  %div
+    %input(type="submit" value="Sign Up")
+    %input(type="reset" value="Clear")
+@@error
+%p Wrong username or password
+%p Please try again!
